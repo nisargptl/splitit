5d1176fb559dd134fb1012ce6d6b0ea0793e5e20
Yuvrajsinh Raulji <yuvrajsinhraulji@yuvrajsinhs-air.dhcp.asu.edu>
2024-09-29 02:26:41 -0700

    user list API added

--- BE/server.js
+++ BE/server.js
@@ -64,6 +64,18 @@ app.post('/api/user', async (req, res) => {
     }
 });
 
+app.get('/api/userList', async (req, res) => {
+    try {
+        const users = await User.find({}, { name: 1 });
+        if (!users) {
+            return res.status(404).json({ message: 'Users not found' });
+        }
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 app.get('/api/user/:id', async (req, res) => {
     try {
         const userId = req.params.id;
