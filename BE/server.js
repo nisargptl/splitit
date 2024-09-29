8142389e1e4e6a5c5d04af7538a6cc84b4c2d87f
Yuvrajsinh Raulji <yuvrajsinhraulji@yuvrajsinhs-air.dhcp.asu.edu>
2024-09-28 18:46:58 -0700

    User CRUD operations added

--- BE/server.js
+++ BE/server.js
@@ -1,13 +1,85 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./Models/userModel');
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Basic route
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+app.post('/api/user', async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+
+        // Create new user
+        const newUser = new User({
+            name,
+            email,
+            password,
+            friends: []
+        });
+
+        // Save the user to the database
+        await newUser.save();
+        res.status(201).json({ message: 'User created successfully', user: newUser });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/api/user/:id', async (req, res) => {
+    try {
+        console.log("AA:",req.params.id);
+        const userId = req.params.id;
+        const user = await User.findById(userId);
+
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            return res.status(400).json({ message: 'Invalid user ID' });
+        }
+        console.log("BB:", user);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.put('/api/user/:id', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body);
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.delete('/api/user/:id', async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.params.id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(204).send(); // No Content response
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 });
 
 // Start the server
